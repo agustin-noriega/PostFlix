8e7389136777b8595925fb9bd0591124770c8f90
Agustín Noriega <57065873+agustin-noriega@users.noreply.github.com>
2020-05-01 20:25:53 -0300

    Add files via upload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -107,37 +107,35 @@
     <title>PostFlix</title>
   </head>
   <body>
-    <div class="container">
-      <header>
 
-        <nav class="navbar navbar-light bg-transparent">
-          <!-- IMAGEN DEL LOGO -->
-          <a title="Logo PostFlix" href="index.php">
+    <header class="container">
+        <nav id="logo" class="navbar navbar-light bg-transparent">
+          <!-- LOGO -->
+          <a title="PostFlix" href="./index.php">
             <img class="logo" src="logo.png" alt="Postflix">
           </a>
-
-          <!-- BARRA DE NAVEGACION -->
-          <form class="form-inline   col-lg-4 col-xs-12">
-            <a title="Boton Login" href="./login">
-              <button class="btn btn-outline-success" style="margin:10px" id="boton" type="button"> Login </button>
-            </a>
-            <a title="Boton Registro" href="./register">
-              <button class="btn btn-outline-success" style="margin:10px" id="boton" type="button"> Registro </button>
-            </a>
-            <a title="Boton Mis Posts" href="./vistapost">
-              <button class="btn btn-outline-success" style="margin:10px" id="boton" type="button"> MisPosts </button>
-            </a>
-            <a title="Boton Ayuda" href="./faqs">
-              <button class="btn btn-outline-success" style="margin:10px" id="boton" type="button"> F.A.Q.S </button>
-            </a>
-            <a title="Boton Contacto" href="./contacto">
-              <button class="btn btn-outline-success" style="margin:10px" id="boton" type="button"> Contacto </button>
-            </a>
-            <!-- <button class="btn btn-sm btn-outline-secondary" type="button">Smaller button</button> -->
-          </form>
         </nav>
-      </header>
-      <!-- INFO DEL SITIO -->
+        <div id="menu" class="form-inline ">
+          <a title="Boton Login" href="./login">
+            <button class="btn btn-outline-success" style="margin:10px" id="boton" type="button"> Login </button>
+          </a>
+          <a title="Boton Registro" href="./register">
+            <button class="btn btn-outline-success" style="margin:10px" id="boton" type="button"> Registro </button>
+          </a>
+          <a title="Boton Mis Posts" href="./vistapost">
+            <button class="btn btn-outline-success" style="margin:10px" id="boton" type="button"> MisPosts </button>
+          </a>
+          <a title="Boton Ayuda" href="./faqs">
+            <button class="btn btn-outline-success" style="margin:10px" id="boton" type="button"> F.A.Q.S </button>
+          </a>
+          <a title="Boton Contacto" href="./contacto">
+            <button class="btn btn-outline-success" style="margin:10px" id="boton" type="button"> Contacto </button>
+          </a>
+        </div>
+    </header>
+
+    <div class="container">
+        <!-- INFO DEL SITIO -->
       <section class="intro-home col-lg-12 col-md-12 col-xs-12 rounded-pill">
         <h1>Postflix!</h1>
         <br>
@@ -149,17 +147,5 @@
 
       </section>
     </div>
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
