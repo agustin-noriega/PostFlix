7890929ca146e9ae97f1260c70534102db7cf0ed
Agustín Noriega <57065873+agustin-noriega@users.noreply.github.com>
2020-05-01 18:49:52 -0300

    Add files via upload

--- resources/views/altaPost.blade.php
+++ resources/views/altaPost.blade.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+  <head class="">
     <link rel="stylesheet" href="css/altapost-style.css" >
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <meta charset="utf-8">
     <title>Postflix Nuevo Post</title>
   </head>
   <body>
-    <header>
+    <header class="container">
       <nav class="navbar navbar-light bg-transparent">
         <!-- LOGO -->
-        <a title="PointFlix" href="./index.php">
+        <a title="PostFlix" href="./index.php">
           <img class="logo" src="logo.png" alt="Postflix">
         </a>
       </nav>
-      <div class="form-inline"   "col-lg-4 col-xs-12">
+      <div class="form-inline  col-xs-2">
         <a title="Boton Home" href="index.php">
-          <button class="btn btn-outline-success" style="margin:10px" id="boton" type="button"> Home </button>
+          <button class="btn btn-outline-success " style="margin:10px" id="boton" type="button"> Home </button>
         </a>
 
         <a title="Boton Login" href="./login">
@@ -34,29 +34,29 @@
         </a>
       </div>
     </header>
-    <section>
-      <form id="formulario" class="form-signin rounded-pill">
-        <p>Ingresar Imagen de tu post</p>
-        <input type="file" id="botoncoment" name="myfile">
-        <p>Descripción</p>
+    <br><br>
+    <section class="container">
+      <section id="alta" class="form-signin rounded-pill col-8 col-md-8 col-lg-12">
+        <p id="imagen-tit" class="col-xs-8 col-md-8">Ingresa la Imagen de tu post</p>
+        <br>
+        <input type="file" id="botonarchivo" name="myfile">
+        <br><br>
+        <p id="descripcion-tit">Descripción</p>
         <textarea id="comentario" placeholder="Escribe aqui la descripción de tu post!..." name="message" rows="4" cols="67"  ></textarea>
+        <div id="box-puntuacion">
+          <p id="puntuacion-tit">Ingresa una puntuacion</p>
+          <select id="puntuacion" class="form-control">
+            <option >Elija una puntuación</option>
+            <option >1</option><option >2</option><option >3</option><option >4</option>
+            <option >5</option><option >6</option><option >7</option><option >8</option>
+            <option >9</option><option >10</option>
+          </select>
+        </div>
+        <br>
 
-        <p>Ingresa una puntuacion</p>
-        <select class="form-control">
-          <option >Seleccione una puntuación</option>
-          <option >1</option><option >2</option><option >3</option><option >4</option>
-          <option >5</option><option >6</option><option >7</option><option >8</option>
-          <option >9</option><option >10</option>
-
-
-        </select>
-        <br>
-        <br>
-        <br>
-        <br>
         <button id="botoncoment" class="btn" type="submit" >Publicar Post</button>
         <br><br>
-      </form>
+      </section>
     </section>
 
   </body>
