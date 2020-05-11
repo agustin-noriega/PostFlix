fda2e26ed3d3b148cc4a5f2c0bb7a3ac3fbea52f
gallol <luciano.gallo@gmail.com>
2020-05-11 19:09:09 -0300

    vistapost borre comment

--- resources/views/vistapost.blade.php
+++ resources/views/vistapost.blade.php
@@ -11,39 +11,6 @@
 
       </head>
       <body>
-        {{-- BARRA ANTERIOR COMENTADA --}}
-  <!--<header>
-      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a href="./index.php">  <img id="postflixlogo" class="mb-4" src="logo.png" alt="" width="120" height="25"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="./index.php">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="./perfil">Perfil</a>
-            </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="./faqs">F.A.Q.S</a>
-            </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="./mis-contactos">Mis contactos</a>
-            </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="./contacto">Ayuda</a>
-            </li>
-          </ul>
-          <form class="form-inline mt-2 mt-md-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Buscar" aria-label="Buscar">
-            <button id="search" class="btn btn-outline-success" type="submit">Buscar</button>
-          </form>
-        </div>
-      </nav>
-    </header>
-    <main role="main">-->
 
     <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
    <a href="./index.php"><img id="postflixlogo" class="mb-4" src="./Imagenes/logo.png" alt="" width="120" height="25"></a>
